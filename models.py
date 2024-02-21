61a663e1d39f2df8ba28de5bbb7ae0f3a0bb6549
AnjiPythonUser <andreyavtogruz@gmail.com>
2024-02-21 11:45:09 +0300

    Готовый проект с использованием фреймворка FastApi

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class Car(Base):
+    __tablename__ = 'cars'
+
+    id = Column(Integer, primary_key=True)
+    brand = Column(String)
+    mark = Column(String)
+    year = Column(Integer)
+
+
